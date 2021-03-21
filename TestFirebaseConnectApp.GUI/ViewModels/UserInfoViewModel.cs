f2c05adcd60b5f8d8edf27688aff14f67a2b346e
Kleozis <Kleozis@mail.ru>
2021-03-21 13:04:54 +0300

    added functionality for "change password" and "delete account"

--- TestFirebaseConnectApp.GUI/ViewModels/UserInfoViewModel.cs
+++ TestFirebaseConnectApp.GUI/ViewModels/UserInfoViewModel.cs
@@ -16,21 +16,66 @@ namespace TestFirebaseConnectApp.GUI.ViewModels
 
         public FirebaseUser User => _mediator.FirebaseAuth.User;
 
-        private RelayCommand _changePasswordCommand;
-        public RelayCommand ChangePasswordCommand =>
-            _changePasswordCommand ??= new RelayCommand(o => 
+        #region change password popup
+        public bool ChangePasswordPopupState { get; set; }
+
+        private RelayCommand _openChangePasswordPopupCommand;
+        public RelayCommand OpenChangePasswordPopupCommand =>
+            _openChangePasswordPopupCommand ??= new RelayCommand(o =>
             {
                 ChangePasswordPopupState = true;
                 OnPropertyChanged("ChangePasswordPopupState");
             });
 
-        private RelayCommand _deleteAccountCommand;
-        public RelayCommand DeleteAccountCommand =>
-            _deleteAccountCommand ??= new RelayCommand(o =>
+        private RelayCommand _confirmChangePasswordCommand;
+        public RelayCommand ConfirmChangePasswordCommand => 
+            _confirmChangePasswordCommand ??= new RelayCommand(o =>
             {
-                // код удаления аккаунта
+                var password = (string)o;
+                var token = _mediator.FirebaseAuth.FirebaseToken;
+                _authProvider.ChangeUserPasswordAsync(token, password);
+
+                ChangePasswordPopupState = false;
+                OnPropertyChanged("ChangePasswordPopupState");
+            });
+
+        private RelayCommand _cancelChangePasswordCommand;
+        public RelayCommand CancelChangePasswordCommand =>
+            _cancelChangePasswordCommand ??= new RelayCommand(o =>
+            {
+                ChangePasswordPopupState = false;
+                OnPropertyChanged("ChangePasswordPopupState");
+            });
+        #endregion
+
+        #region delete account popup
+        public bool ConfirmDeleteAccountPopupState { get; set; }
+
+        private RelayCommand _openDeleteAccountPopupCommand;
+        public RelayCommand OpenDeleteAccountPopupCommand =>
+            _openDeleteAccountPopupCommand ??= new RelayCommand(o =>
+            {
+                ConfirmDeleteAccountPopupState = true;
+                OnPropertyChanged("ConfirmDeleteAccountPopupState");
+            });
+
+        private RelayCommand _confirmDeleteAccountCommand;
+        public RelayCommand ConfirmDeleteAccountCommand =>
+            _confirmDeleteAccountCommand ??= new RelayCommand(o =>
+            {
+                var token = _mediator.FirebaseAuth.FirebaseToken;
+                _authProvider.DeleteUserAsync(token);
                 _mediator.Notify(this, "delete");
             });
+
+        private RelayCommand _cancelDeleteAccountCommand;
+        public RelayCommand CancelDeleteAccountCommand =>
+            _cancelDeleteAccountCommand ??= new RelayCommand(o =>
+            {
+                ConfirmDeleteAccountPopupState = false;
+                OnPropertyChanged("ConfirmDeleteAccountPopupState");
+            });
+        #endregion
 
         private RelayCommand _exitFromAccountCommand;
         public RelayCommand ExitFromAccountCommand =>
@@ -38,11 +83,5 @@ namespace TestFirebaseConnectApp.GUI.ViewModels
             {
                 _mediator.Notify(this, "exit");
             });
-
-
-        public bool ChangePasswordPopupState { get; set; }
-
-        public bool ConfirmDeleteAccountPopupState { get; set; }
-
     }
 }
