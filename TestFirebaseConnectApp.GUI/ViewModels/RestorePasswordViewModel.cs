f9e30d25bbd4c07f417c5e1762456385eb5b497a
Kleozis <Kleozis@mail.ru>
2021-03-21 19:08:51 +0300

    added email validation for RestorePasswordView

--- TestFirebaseConnectApp.GUI/ViewModels/RestorePasswordViewModel.cs
+++ TestFirebaseConnectApp.GUI/ViewModels/RestorePasswordViewModel.cs
@@ -15,8 +15,27 @@ namespace TestFirebaseConnectApp.GUI.ViewModels
             _restorePasswordCommand ??= new RelayCommand(async o => 
             {
                 var email = (string)o;
-                await _authProvider.SendPasswordResetEmailAsync(email); // переделать
-                _mediator.Notify(this, "restorePassword");
+
+                var errorText = string.Empty;
+                if (!EmailValidator.Validate(email, ref errorText))
+                {
+                    MessagePopupText = errorText;
+                    MessagePopupState = true;
+                    return;
+                }
+
+                try
+                {
+                    await _authProvider.SendPasswordResetEmailAsync(email);
+                    MessagePopupText = "Письмо со сменой пароля отправлено на почту.";
+                    MessagePopupState = true;
+                }
+                catch (FirebaseAuthException fae) when (fae.Reason == AuthErrorReason.UnknownEmailAddress)
+                {
+                    MessagePopupText = "Пользователь с такой почтой не найден.";
+                    MessagePopupState = true;
+                }
+
             });
 
         private RelayCommand _goToLoginViewCommand;
@@ -33,5 +52,36 @@ namespace TestFirebaseConnectApp.GUI.ViewModels
             {
                 _mediator.Notify(this, "goRegister");
             });
+
+        #region popup
+        private bool _messagePopupState;
+        public bool MessagePopupState
+        {
+            get => _messagePopupState;
+            set
+            {
+                _messagePopupState = value;
+                OnPropertyChanged("MessagePopupState");
+            }
+        }
+
+        private string _messagePopupText;
+        public string MessagePopupText
+        {
+            get => _messagePopupText;
+            set
+            {
+                _messagePopupText = value;
+                OnPropertyChanged("MessagePopupText");
+            }
+        }
+
+        private RelayCommand _closePopupCommand;
+        public RelayCommand ClosePopupCommand =>
+            _closePopupCommand ??= new RelayCommand(o =>
+            {
+                MessagePopupState = false;
+            });
+        #endregion
     }
 }
